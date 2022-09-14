04e229b0fa0b3c0fa67282335c7b7cc6569061be
Shimul <shimul.audacityit@bkash.com>
2022-09-14 10:59:38 +0600

    Initial Commit

---
+++ Posts/PostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostsApp.swift
+//  Posts
+//
+//  Created by Abdullah Al Mamur on 9/14/22.
+//
+
+import SwiftUI
+
+@main
+struct PostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
